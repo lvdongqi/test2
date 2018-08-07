f0ae31d76dd8355d372f2f6a3b535f437299b517
“lvdongqi <1040265512@qq.com>
2018-08-07 17:22:23 +0800

    ceshi

---
+++ src/main/java/com/jk/service/TestService.java
@@ -0,0 +1,12 @@
+package com.jk.service;
+
+/**
+ * @author 吕东崎
+ * @Title: TestService
+ * @ProjectName springboot-provider
+ * @Description: TODO
+ * @date 2018/8/715:40
+ */
+public interface TestService {
+    String sayHello();
+}
